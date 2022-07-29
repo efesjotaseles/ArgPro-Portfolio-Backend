b6cb20596599fba728aa1eaa5f13966efb02e26a
Fede <efesjotaseles2@gmail.com>
2022-07-28 21:11:47 -0300

    Minor update

--- src/main/java/com/portfoliobackend/portfoliobackend/controller/portfolioController.java
+++ src/main/java/com/portfoliobackend/portfoliobackend/controller/portfolioController.java
@@ -1,10 +1,14 @@
 package com.portfoliobackend.portfoliobackend.controller;
 
+import com.portfoliobackend.portfoliobackend.model.Experiencia;
 import com.portfoliobackend.portfoliobackend.service.AcercaDeService;
 import com.portfoliobackend.portfoliobackend.service.EstudioService;
 import com.portfoliobackend.portfoliobackend.service.ExperienciaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class portfolioController {
@@ -15,5 +19,10 @@ public class portfolioController {
     private EstudioService estudioService;
     @Autowired
     private AcercaDeService acercaDeService;
+
+    @GetMapping("/experiencias")
+    public List<Experiencia> getExperiencias(){
+        return experienciaService.getExperiencias();
+    }
 
 }
